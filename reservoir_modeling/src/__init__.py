8e5d0a60ff4bc33416ae9053584828c15e5c2898
Alexey0107 <bahmutov2002@gmail.com>
2025-05-06 13:12:04 +0300

    Task2_13

---
+++ reservoir_modeling/src/__init__.py
@@ -0,0 +1,5 @@
+from .well_analysis import NodalAnalysis, plot_combined
+from .inflow_performance import InflowWell, InvertedWell
+from .outflow_performance import VLPModel
+
+__all__ = ["NodalAnalysis", "plot_combined", "InflowWell", "InvertedWell", "VLPModel"]
